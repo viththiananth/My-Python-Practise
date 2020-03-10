01363f92652bd8ee291605813b651b1024f41706
viththi <56210820+viththiananth@users.noreply.github.com>
2020-03-10 05:48:40 +0530

    String List

--- 6. String Lists.py
+++ 6. String Lists.py
@@ -15,3 +15,15 @@ else :
 def reversed_word(word):
     x=''
     for i in range(len(word)):
+        x+=word[len(word)-1-i]
+    return x
+
+
+word=input("Please Input Second Text")
+a=reversed_word(word)
+print(a)
+
+if a==word:
+    print('This is a Palindrome')
+else:
+    print('This is not a Palindrome')
