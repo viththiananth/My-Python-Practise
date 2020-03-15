b44f552bcf43b650f375c0b27f9cb53a32993c5f
viththi <56210820+viththiananth@users.noreply.github.com>
2020-03-15 11:07:27 +0530

    Added Practise 14

--- 14. List Remove Duplicates.py
+++ 14. List Remove Duplicates.py
@@ -4,3 +4,22 @@
 
 #Write two different functions to do this - one using a loop and constructing a list, and another using sets.
 #Go back and do Exercise 5 using sets, and write the solution for that in a different function.
+
+
+
+
+def unique_list(list1):
+    a= set(list1)
+    return a
+
+def unique_list_loop(list1):
+    y=[]
+    for i in list1:
+        if i not in y:
+            y.append(i)
+    return y
+
+lst1=[1,2,2,2,2,4,5,5,5,6,6,7,8]
+
+print(unique_list(lst1))
+print(unique_list_loop(lst1))
